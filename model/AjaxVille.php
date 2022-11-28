89812818c040b0a0784f6d90a3c6ffa90ae00f6d
Julien <julien.dousse@gmail.com>
2022-11-28 10:06:36 +0100

    add AjaxVille

---
+++ model/AjaxVille.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../../config/Database.php';
+
+require_once '../../Model/Ville.php';
+
+if (!empty($_POST["departement"])) {
+
+    Ville::GetVilleByIdDepartement($_POST["departement"]);
+}
