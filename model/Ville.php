f6260b74b6f2d547cab027c729b2552f7ac90fb9
Julien <julien.dousse@gmail.com>
2022-11-28 09:58:31 +0100

    creation Model Ville

---
+++ model/Ville.php
@@ -0,0 +1,131 @@
+<?php
+
+class Ville
+{
+    private int $idVille;
+    private string $nomVille;
+    private int $codeVile;
+    private int $CodeInsee;
+    private int $idDepartement;
+
+
+
+    public function __construct($idVille, $nomVille, $CodeInsee, $CodeVille, $idDepartement)
+    {
+        $this->idVille = $idVille;
+        $this->CodeVille = $CodeVille;
+        $this->nomVille = $nomVille;
+        $this->idVille = $CodeInsee;
+        $this->nomVille = $idDepartement;
+    }
+
+    /**
+     * Get the value of idVille
+     * 
+     * @return int
+     */
+    public function getIdVille(): int
+    {
+        return $this->idVille;
+    }
+
+    /**
+     * Set the value of idVille
+     *
+     * @return  self
+     */
+    public function setIdVille($idVille): self
+    {
+        $this->idVille = $idVille;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nomVille
+     * 
+     * @return string
+     */
+    public function getNomVille(): string
+    {
+        return $this->nomVille;
+    }
+
+    /**
+     * Set the value of nomVille
+     *
+     * @return  self
+     */
+    public function setNomVille($nomVille): self
+    {
+        $this->nomVille = $nomVille;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of codeVile
+     * 
+     * @return int
+     */
+    public function getCodeVile(): int
+    {
+        return $this->codeVile;
+    }
+
+    /**
+     * Set the value of codeVile
+     *
+     * @return  self
+     */
+    public function setCodeVile($codeVile): self
+    {
+        $this->codeVile = $codeVile;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of CodeInsee
+     * 
+     * @return int
+     */
+    public function getCodeInsee(): int
+    {
+        return $this->CodeInsee;
+    }
+
+    /**
+     * Set the value of CodeInsee
+     *
+     * @return  self
+     */
+    public function setCodeInsee($CodeInsee): self
+    {
+        $this->CodeInsee = $CodeInsee;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idDepartement
+     * 
+     * @return int
+     */
+    public function getIdDepartement(): int
+    {
+        return $this->idDepartement;
+    }
+
+    /**
+     * Set the value of idDepartement
+     *
+     * @return  self
+     */
+    public function setIdDepartement($idDepartement): self
+    {
+        $this->idDepartement = $idDepartement;
+
+        return $this;
+    }
+}
